230304b5e39a11827eb906c0397303bc7e48afa6
Andrewwss <andriislichenko@gmail.com>
2021-10-16 18:24:25 +0300

    update firebase (stored users)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shoppage/shop.component";
 import Header from "./componetns/header/header.component";
 import SignInAndSignUpComponent from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import {
+  auth,
+  createUserProfileDocument,
+} from './firebase/firebase.utils';
 
 class App extends React.Component {
   constructor() {
@@ -19,10 +22,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    auth.onAuthStateChanged(user => {
-      this.setState({ currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-      console.log(user);
+        userRef.onSnapshot(snapshot => {
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          }, () => {
+            console.log(this.state);
+          })
+        });
+
+      } else {
+        this.setState({currentUser: userAuth})
+      }
     })
   }
 
