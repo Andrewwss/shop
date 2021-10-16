0192be48135220f696046ddaebb0a0c8257a9477
Andrewwss <andriislichenko@gmail.com>
2021-10-16 20:49:37 +0300

    implement sign up with email

--- src/App.js
+++ src/App.js
@@ -26,21 +26,19 @@ class App extends React.Component {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapshot => {
+        userRef.onSnapshot(snapShot => {
           this.setState({
             currentUser: {
-              id: snapshot.id,
-              ...snapshot.data()
+              id: snapShot.id,
+              ...snapShot.data()
             }
-          }, () => {
-            console.log(this.state);
-          })
-        });
+          });
 
-      } else {
-        this.setState({currentUser: userAuth})
+          console.log(this.state);
+        });
       }
-    })
+      this.setState({currentUser: userAuth})
+    });
   }
 
   componentWillUnmount() {
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
 import SignIn from "../../componetns/sign-in/sign-in.component";
-
+import SignUp from "../../componetns/sign-up/sign-up.component";
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpComponent = () => (
   <div className='sign-in-and-sign-up'>
     <SignIn />
+    <SignUp />
   </div>
 );
 
